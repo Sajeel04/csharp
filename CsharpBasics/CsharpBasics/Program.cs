3dff4cb36f86c3778f4eb5f388763c72b7a56612
Sajeel04 <91761902+Sajeel04@users.noreply.github.com>
2022-08-26 14:28:20 +0200

    init

---
+++ CsharpBasics/CsharpBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CsharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Once upon a time");
+            Console.WriteLine("I was falling in love");
+            Console.WriteLine("Now I’m only falling apart");
+            
+            // övning 5 
+            string text = "Hej Ada LoveLace";
+            Console.WriteLine(text);
+
+            //övning 6
+            string svar = Console.ReadLine();
+            Console.WriteLine(svar);
+
+            // övning 7
+            string answer = Console.ReadLine();
+            Console.WriteLine(answer);
+            Console.WriteLine(answer);
+            Console.WriteLine(answer);
+            // övning 8
+            Console.WriteLine("What is your name");
+            string question = Console.ReadLine();
+            Console.WriteLine(question);
+            Console.WriteLine("Välkommen " + question);
+            //övning 9 
+            string yrke = Console.ReadLine();
+            Console.WriteLine("Hej, hur mår du");
+            Console.WriteLine("Vad har du att säga " + yrke);
+           
+
+        }
+    }
+}
